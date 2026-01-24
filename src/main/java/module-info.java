c8557ed8593e9f141e68dbfdbccec2e7e5fc4aee
RealMangoRage <64402114+RealMangorage@users.noreply.github.com>
2026-01-23 18:43:55 -0800

    Fixed module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,6 @@ module org.mangorage.mangobotgithub {
     requires okhttp3;
     requires org.eclipse.egit.github.core;
     requires org.kohsuke.github.api;
-    requires org.mangorage.mangobotwebsite;
     requires org.mangorage.mangobotplugin;
     requires org.mangorage.mangobotcore;
     requires net.dv8tion.jda;
